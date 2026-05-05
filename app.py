89fa14d06f7d7be961f1fb48d615509064649772
apidial-code <apidial@gmail.com>
2026-05-06 08:38:34 +1000

    Fix formatting issues in send_first_test_email function

--- app.py
+++ app.py
@@ -372,6 +372,8 @@ def send_brevo_email(to_email, to_name, subject, html_content):
 def send_first_test_email(data):
     try:
         color = "#e74c3c"
+        completed_test_label = f"Year {data.get('test_grade', '')}"
+        next_test_label = "Next Test"
         student_full_name = data["student_name"]
         student_first_name = student_full_name.split()[0] if student_full_name else "Student"
 
